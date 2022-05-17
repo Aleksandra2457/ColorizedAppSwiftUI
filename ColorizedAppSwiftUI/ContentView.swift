d3451eea3f901ba032ae79032dce5c40b0ff1e27
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-05-17 15:13:49 +0300

    User can change rectangle's color with sliders. All the labels show the current RGB component.

--- ColorizedAppSwiftUI/ContentView.swift
+++ ColorizedAppSwiftUI/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redSliderValue = 70.0
+    @State private var greenSliderValue = 145.0
+    @State private var blueSliderValue = 235.0
+    
+    @State private var redSliderTextValue = "00.0"
+    @State private var greenSliderTextValue = "00.0"
+    @State private var blueSliderTextValue = "00.0"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            ColorRectangleView(color: Color(red: redSliderValue / 255, green: greenSliderValue / 255, blue: blueSliderValue / 255))
+            HorizontalStackView(sliderValue: $redSliderValue, sliderValueText: $redSliderTextValue, sliderColor: .red)
+            HorizontalStackView(sliderValue: $greenSliderValue, sliderValueText: $greenSliderTextValue, sliderColor: .green)
+            HorizontalStackView(sliderValue: $blueSliderValue, sliderValueText: $blueSliderTextValue, sliderColor: .blue)
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 50, leading: 16, bottom: 16, trailing: 16))
     }
+        
 }
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct HorizontalStackView: View {
+    
+    @Binding var sliderValue: Double
+    @Binding var sliderValueText: String
+    
+    let sliderColor: Color
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            Text(String(format: "%.0f", arguments: [sliderValue]))
+                .frame(width: 40)
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+                .tint(sliderColor)
+            TextField("255", text: $sliderValueText)
+                .frame(width: 60)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
+        }
+        .frame(width: UIScreen.main.bounds.width - 32)
+    }
+    
 }
